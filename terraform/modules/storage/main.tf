4c7c8a5b00ad3afd591024b97e17b3e7a24cf0de
bertrand76-fr <54433710+bertrand76-fr@users.noreply.github.com>
2025-08-28 02:23:35 +0200

    Correction du nom de staorage

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "main" {
-  name                     = "${var.storage_account_prefix}-${var.resource_group_name}"
+  name                     = "${var.storage_account_prefix}${var.resource_group_name}"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
