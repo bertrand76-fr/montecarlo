4e7950f8644872f30d7fc8313e89ef26a9579f88
bertrand76-fr <54433710+bertrand76-fr@users.noreply.github.com>
2025-08-28 18:31:57 +0200

    maj du pipeline pour setter et executer la fonction

--- terraform/modules/producer/outputs.tf
+++ terraform/modules/producer/outputs.tf
@@ -1,9 +1,9 @@
-output "name" {
+output "producer_name" {
   description = "Nom de la Function App"
   value       = azurerm_function_app.main.name
 }
 
-output "default_hostname" {
+output "producer_default_hostname" {
   description = "Endpoint de la Function App"
   value       = azurerm_function_app.main.default_hostname
 }
